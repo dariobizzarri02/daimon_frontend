349a1b9310a81b538d2d3a7f4cf11367bf16fc8e
Dario Bizzarri <96698836+mbentity@users.noreply.github.com>
2025-04-25 14:11:04 +0200

    temporarily expose masterbase canon repo

--- app/universe/[...article]/page.tsx
+++ app/universe/[...article]/page.tsx
@@ -18,7 +18,7 @@ export default function Universe (props: { params: Params }) {
 
     useEffect(() => {
         const fullPath = params.article.join("/");
-        const repoName = "daimon_canon";
+        const repoName = "daimon_canon_masterbase";
         const repoOwner = "masterbaseguild";
         const branchName = "live";
         const filePath = `${fullPath}.md`;
